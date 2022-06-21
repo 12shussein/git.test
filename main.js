f6084b8c8056334c9c779ac31ef5fb6b182ed083
Abdirahim Hussein <drahimhussein@gmail.com>
2022-06-21 16:06:06 +0100

    my first webpage commit using git

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var Pictures;
 
 function Setvalue() {
   if(--window.LoopTrap <= 0) throw "Infinite loop.";
-  Pictures = ['https://flipdish.imgix.net/RsOTh5AS3M2UgNRBc5RMJa8MtEk.jpg?w=1080&h=720', 'https://flipdish.imgix.net/lg0VX3TRcFuOkFgyyyRl1upxDGk.jpg?w=1080&h=720', 'https://flipdish.imgix.net/sJLVKZ8THjrMg4RZUiEMF56Kqg.jpg?w=1080&h=720', 'https://flipdish.imgix.net/mfratMYj6hYLqcP9EwG21RkD8E.jpg?w=1080&h=720', 'https://flipdish.imgix.net/qjeYXXaWdTmpQa0op5IVlQxzk7o.jpg?w=1080&h=720', 'https://flipdish.imgix.net/VCmvoxgl7hjwnYMTRovTWTWdjU.jpg?w=1080&h=720', 'https://i.etsystatic.com/24305988/r/il/274a22/2462601398/il_794xN.2462601398_lsge.jpg'];
+  Pictures = ['https://flipdish.imgix.net/RsOTh5AS3M2UgNRBlsc5RMJa8MtEk.jpg?w=1080&h=720', 'https://flipdish.imgix.net/lg0VX3TRcFuOkFgyyyRl1upxDGk.jpg?w=1080&h=720', 'https://flipdish.imgix.net/sJLVKZ8THjrMg4RZUiEMF56Kqg.jpg?w=1080&h=720', 'https://flipdish.imgix.net/mfratMYj6hYLqcP9EwG21RkD8E.jpg?w=1080&h=720', 'https://flipdish.imgix.net/qjeYXXaWdTmpQa0op5IVlQxzk7o.jpg?w=1080&h=720', 'https://flipdish.imgix.net/VCmvoxgl7hjwnYMTRovTWTWdjU.jpg?w=1080&h=720', 'https://i.etsystatic.com/24305988/r/il/274a22/2462601398/il_794xN.2462601398_lsge.jpg'];
 }
 
 
